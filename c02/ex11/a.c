7d8fa2f075a366402d746156036d167b2f4a1545
Salih Karagollu <skaragol@k1m22s02.42kocaeli.com.tr>
2022-02-20 10:39:14 +0300

    temizlik

--- c02/ex11/a.c
+++ c02/ex11/a.c
@@ -1,41 +1,65 @@
 #include <unistd.h>
 
-int hex(int a)
-{
-	char sayi[3];
-	int i;
+char g_sayi[255];
+
+void hex(int a)
+{	int i;
 	int rem;
 
-	sayi[0]='\\';
-	sayi[1]='0'
+	g_sayi[0]='\\';
+	g_sayi[1]='0';
 	i=0;
-		rem=sayi%16
-		if(rem<10)
+		rem=a%16;
+		if(a<10)
 		{
-			sayi[3]=rem+48;
+			g_sayi[2]=a+48;
 		}
-		else
+		else if (a < 16)
 		{
-			sayi[2]=rem+55;
-		}
-	}
+			g_sayi[2]=a+87;
+		} else
+		{
+			g_sayi[2]=1;
+			a=a/16;
+			if(a<10)
+			{
+				g_sayi[2]=a+48;
+			}
+			else if (a < 16)
+			{
+				g_sayi[2]=a+87;
 
-	return ;
+			}
+
+		}
 }
-
 void    ft_putstr(char *str)
 {
     int i;
+	int j;
     i = 0;
     while (str[i] != '\0')
-    {
-		if(str[i]>=32&&str[i]<=127){
+    {	
+		j=0;
+		if(str[i]>=32&&str[i]<=127)
         write (1, &str[i], 1);
-		else if(str[i]<32)
+		else if(str[i]<32){
+			hex(str[i]);
+		while(j<3)
 		{
-		
+			write(1,&g_sayi[i], 1);
+			j++;
+		}}
+		i++;
 		}
-        i++;
+
 		}
-    }
+    
+
+
+
+int main()
+{
+	ft_putstr("Merhaba\nBenim");
+	return 0;
 }
