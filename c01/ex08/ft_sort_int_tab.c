e511a9a51f727929d15cc3f08d478c4676fc39bd
Salih Karagollu <skaragol@k1m13s02.42kocaeli.com.tr>
2022-02-21 20:43:31 +0300

    bunuokuyan tosun okuyana kosun

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,23 +1,37 @@
-void   ft_sort_int_tab(int *tab, int size)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skaragol <skaragol@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 20:32:26 by skaragol          #+#    #+#             */
+/*   Updated: 2022/02/21 20:32:28 by skaragol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
 {
-   int     count;
-   int     i;
-   int     temp;
-   i = 0;
-   count = 1;
-   while (count != 0)
-   {
-	   i=0;
-	   count = 0;
-	   while(i!=size-1){
-	   if(tab[i] > tab[i+1])
-	   {
-		   temp = tab[i];
-		   tab[i] = tab[i + 1];
-		   tab[i + 1] = temp;
-		   count++;
-	   }
-	   i++;
-	   }
-   }
+	int	count;
+	int	i;
+	int	temp;
+
+	i = 0;
+	count = 1;
+	while (count != 0)
+	{
+		i = 0;
+		count = 0;
+		while (i != size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				count++;
+			}
+			i++;
+		}
+	}
 }
